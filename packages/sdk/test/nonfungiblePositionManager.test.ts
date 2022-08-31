44bc0440c9a9e5dc8c6fbcdb7488d60b23156220
ICONationDevTeam <iconation.team@gmail.com>
2022-08-31 18:41:22 +0200

    Provide a better example for liquidity for minting

--- packages/sdk/test/nonfungiblePositionManager.test.ts
+++ packages/sdk/test/nonfungiblePositionManager.test.ts
@@ -1,10 +1,13 @@
-import { Token, Icx, Percent, CurrencyAmount } from '@convexus/sdk-core'
-// import { Percent, Token, CurrencyAmount, Icx } from '@convexus/sdk-core'
+import JSBI from 'jsbi'
+import { Token, Icx, Percent, CurrencyAmount, Price, MaxUint256 } from '@convexus/sdk-core'
 import { FeeAmount, TICK_SPACINGS } from '../src/constants'
 import { Pool } from '../src/entities/pool'
 import { Position } from '../src/entities/position'
 import { NonfungiblePositionManager } from '../src/nonfungiblePositionManager'
 import { encodeSqrtRatioX96 } from '../src/utils/encodeSqrtRatioX96'
+import { maxLiquidityForAmounts } from '../src/utils/maxLiquidityForAmounts'
+import { priceToClosestTick } from '../src/utils/priceTickConversions'
+import { nearestUsableTick } from '../src/utils/nearestUsableTick'
 
 describe('NonfungiblePositionManager', () => {
   const token0 = new Token('cx0000000000000000000000000000000000000001', 18, 't0', 'token0')
@@ -16,7 +19,6 @@ describe('NonfungiblePositionManager', () => {
   const WICX = ICX.wrapped
 
   const pool_0_1 = new Pool(token0, token1, fee, encodeSqrtRatioX96(1, 1), 0, 0, [])
-  const pool_1_2 = new Pool(token1, token2, fee, encodeSqrtRatioX96(6, 10), 0, -0x13f5, [])
   const pool_1_wicx = new Pool(token1, WICX, fee, encodeSqrtRatioX96(1, 1), 0, 0, [])
 
   const recipient = 'hx0000000000000000000000000000000000000003'
@@ -91,13 +93,44 @@ describe('NonfungiblePositionManager', () => {
       expect(value).toEqual('0x0')
     })
 
-    it('succeeds for mint pool 2', () => {
+    it('succeeds for mint pool CRV/WETH', () => {
+      // https://i.imgur.com/xTZj9Hp.png
+
+      const highFee = FeeAmount.HIGH // 1% fee
+
+      // Current Price: 1364 CRV per WETH
+      const pool_1_2_price = JSBI.BigInt("2926833475291797893971652918731") // ~ equals to encodeSqrtRatioX96(1364, 1)
+      const pool_1_2_liquidity = JSBI.BigInt("270755706369106903362800")
+      const pool_1_2_tick = 72190
+      const pool_1_2 = new Pool(token1, token2, highFee, pool_1_2_price, pool_1_2_liquidity, pool_1_2_tick, [])
+
+      // Provide liquidity between 953 and 1843
+      const lowerBoundPrice = 953
+      const higherBoundPrice = 1843
+
+      const EXA = JSBI.BigInt("1000000000000000000")
+      const amount0 = JSBI.multiply(EXA, JSBI.BigInt("1")) // 1 ETH provided
+      const amount1 = MaxUint256 // should result in ~1605 CRV
+
+      // Compute the max liquidity for 1 ETH
+      const liquidity = maxLiquidityForAmounts (
+        pool_1_2_price, // Pool price
+        encodeSqrtRatioX96(lowerBoundPrice, 1), // lower bound price
+        encodeSqrtRatioX96(higherBoundPrice, 1), // upper bound price
+        amount0, amount1, // amounts of token provided
+        true
+      )
+
+      // Compute lower & upper ticks from prices
+      const tickLower = nearestUsableTick(priceToClosestTick(new Price(token1, token2, 1, lowerBoundPrice)), TICK_SPACINGS[highFee])
+      const tickUpper = nearestUsableTick(priceToClosestTick(new Price(token1, token2, 1, higherBoundPrice)), TICK_SPACINGS[highFee])
+
       const { calldata, value } = NonfungiblePositionManager.addCallParameters(
         new Position({
           pool: pool_1_2,
-          tickLower: -TICK_SPACINGS[FeeAmount.MEDIUM],
-          tickUpper: TICK_SPACINGS[FeeAmount.MEDIUM],
-          liquidity: 0x10000
+          tickLower: tickLower,
+          tickUpper: tickUpper,
+          liquidity: liquidity
         }),
         { recipient, slippageTolerance, deadline }
       )
@@ -109,15 +142,15 @@ describe('NonfungiblePositionManager', () => {
             "method": "mint",
             "params": {
               "params": {
-                "amount0Desired": "0x18a",
-                "amount0Min": "0x18a",
-                "amount1Desired": "0x0",
-                "amount1Min": "0x0",
+                "amount0Desired": "0xde5e8740bb55a8a",
+                "amount0Min": "0xd67824c75ca970c",
+                "amount1Desired": "0x5726f15f7a74a5b555",
+                "amount1Min": "0x547e5f6be63db8890e",
                 "deadline": "0x7b",
-                "fee": "0xbb8",
+                "fee": "0x2710",
                 "recipient": "hx0000000000000000000000000000000000000003",
-                "tickLower": "-0x3c",
-                "tickUpper": "0x3c",
+                "tickLower": "0x10bf8",
+                "tickUpper": "0x125c0",
                 "token0": "cx0000000000000000000000000000000000000002",
                 "token1": "cx0000000000000000000000000000000000000003"
               }
