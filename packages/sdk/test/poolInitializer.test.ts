c7964b8678428a84b602262c53623be88545d962
ICONationDevTeam <iconation.team@gmail.com>
2022-09-18 14:44:10 +0200

    Add comments to PoolInitializer

--- packages/sdk/test/poolInitializer.test.ts
+++ packages/sdk/test/poolInitializer.test.ts
@@ -5,13 +5,23 @@ import { PoolInitializer } from '../src/poolInitializer'
 import { encodeSqrtRatioX96 } from '../src/utils/encodeSqrtRatioX96'
 
 describe('PoolInitializer', () => {
+  // Create two tokens
   const token0 = new Token('cx0000000000000000000000000000000000000001', 18, 't0', 'token0')
   const token1 = new Token('cx0000000000000000000000000000000000000002', 18, 't1', 'token1')
+  // Medium fee (0.3%)
   const fee = FeeAmount.MEDIUM
-  const pool_0_1 = new Pool(token0, token1, fee, encodeSqrtRatioX96(1, 1), 0, 0, [])
+  // Default pool price : 1 token0 = 1 token1 (1:1)
+  const poolPrice = encodeSqrtRatioX96(1, 1)
+  // Initial liquidity : 0
+  const liquidity = 0
+  // Current tick doesn't need to be computed for deployment
+  const tickCurrent = 0
+  // Create a new pool
+  const pool_0_1 = new Pool(token0, token1, fee, poolPrice, liquidity, tickCurrent)
 
   describe('#createCallParameters', () => {
     it('succeeds', () => {
+      // Pool initializer needs a Pool instance
       const { calldata, value } = PoolInitializer.createCallParameters(pool_0_1)
 
       expect(calldata).toStrictEqual(
@@ -28,6 +38,8 @@ describe('PoolInitializer', () => {
           }
         ]
       )
+      
+      // No ICX needed to be sent
       expect(value).toEqual('0x0')
     })
   })
