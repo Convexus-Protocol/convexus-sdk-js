50fe6c3638a8dbfba1b7df0b879530c80dc37d52
ICONationDevTeam <iconation.team@gmail.com>
2022-09-30 19:02:33 +0200

    Implement QuoteResult

--- packages/sdk/src/entities/results.ts
+++ packages/sdk/src/entities/results.ts
@@ -1,3 +1,6 @@
+import { BigintIsh } from '@convexus/icon-toolkit'
+import JSBI from 'jsbi'
+
 export class NextInitializedTickWithinOneWordResult {
   public readonly tickNext: number
   public readonly initialized: boolean
@@ -14,6 +17,30 @@ export class NextInitializedTickWithinOneWordResult {
     return new NextInitializedTickWithinOneWordResult (
       parseInt(data['tickNext'], 16),
       Boolean(parseInt(data['initialized'], 16)),
+    )
+  }
+}
+
+export class QuoteResult {
+  public readonly amountOut: JSBI;
+  public readonly sqrtPriceX96After: JSBI;
+  public readonly initializedTicksCrossed: number;
+
+  public constructor (
+    amountOut: BigintIsh,
+    sqrtPriceX96After: BigintIsh,
+    initializedTicksCrossed: number
+  ) {
+    this.amountOut = JSBI.BigInt(amountOut)
+    this.sqrtPriceX96After = JSBI.BigInt(sqrtPriceX96After)
+    this.initializedTicksCrossed = initializedTicksCrossed
+  }
+  
+  public static fromCall (data: any): QuoteResult {
+    return new QuoteResult (
+      data['amountOut'],
+      data['sqrtPriceX96After'],
+      parseInt(data['initializedTicksCrossed'], 16),
     )
   }
 }
