233f0241e9ee3bc8a5f1a6b4338860551a70e280
ICONationDevTeam <iconation.team@gmail.com>
2022-10-04 21:33:24 +0200

    Add parseFeeAmount

--- packages/sdk/src/constants.ts
+++ packages/sdk/src/constants.ts
@@ -1,3 +1,6 @@
+import { BigintIsh } from "@convexus/icon-toolkit"
+import JSBI from 'jsbi'
+
 export const ADDRESS_ZERO = 'hx0000000000000000000000000000000000000000'
 
 /**
@@ -8,6 +11,16 @@ export enum FeeAmount {
   LOW = 500,
   MEDIUM = 3000,
   HIGH = 10000
+}
+
+export function parseFeeAmount (s: BigintIsh): FeeAmount {
+  switch (JSBI.BigInt(s).toString()) {
+    case "100": return FeeAmount.LOWEST;
+    case "500": return FeeAmount.LOW;
+    case "3000": return FeeAmount.MEDIUM;
+    case "10000": return FeeAmount.HIGH;
+    default: throw Error("Invalid fee amount");
+  }
 }
 
 /**
