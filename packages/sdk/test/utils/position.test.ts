1663aa1177c24a52fb6a3625de6cfc865f2b9a21
ICONationDevTeam <iconation.team@gmail.com>
2022-10-10 14:10:19 +0200

    Add getKey test

--- packages/sdk/test/utils/position.test.ts
+++ packages/sdk/test/utils/position.test.ts
@@ -22,4 +22,13 @@ describe('PositionLibrary', () => {
       expect(tokensOwed1).toEqual(JSBI.BigInt(1))
     })
   })
+
+  describe('#getKey', () => {
+    it('SHA3256', () => {
+      expect(PositionLibrary.getKey(
+        "cx0000000000000000000000000000000000000001", 
+        123, 456
+      )).toEqual('0xa8f446f2bcc0799eb6b025c457e4638f11bbfaa62180d4b64be3e72b6cf1d54c')
+    })
+  })
 })
