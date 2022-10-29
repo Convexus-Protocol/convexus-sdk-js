4bf1b7a1ae27027fe4cbae63cd5886fbd0e2ac60
ICONationDevTeam <iconation.team@gmail.com>
2022-10-29 13:13:18 +0200

    Add createAndMintCallParameters

--- packages/sdk/src/poolInitializer.ts
+++ packages/sdk/src/poolInitializer.ts
@@ -1,6 +1,7 @@
-import { CallData, Interface, toHex } from '@convexus/icon-toolkit'
+import { BigintIsh, CallData, Interface, toHex, validateAndParseAddress } from '@convexus/icon-toolkit'
+import { Icx, Token } from '@convexus/sdk-core'
 import IPoolInitializer from './artifacts/contracts/PoolInitializer/PoolInitializer.json'
-import { Pool } from './entities'
+import { Pool, Position } from './entities'
 
 export abstract class PoolInitializer {
   public static INTERFACE: Interface = new Interface(IPoolInitializer, "PoolInitializer")
@@ -22,8 +23,54 @@ export abstract class PoolInitializer {
       toHex(pool.sqrtRatioX96)
     ])
   }
+  
+  public static encodeDeposit (token: Token, amount: BigintIsh): CallData {
+    if (Icx.isWrappedAddress(token.address)) {
+      return PoolInitializer.INTERFACE.encodeFunctionDataPayable(
+        amount,
+        'depositIcx', []
+      )
+    } else {
+      return PoolInitializer.INTERFACE.encodeTokenFallbackFunctionData(
+        token.address,
+        amount,
+        'deposit', [], []
+      )
+    }
+  }
+  
+  static encodeCreateAndMint (position: Position, recipient: string, deadline: number): CallData {
+    const pool = position.pool
+    const {amount0, amount1} = position.mintAmounts
+    
+    return PoolInitializer.INTERFACE.encodeFunctionData('createAndInitializePoolIfNecessaryAndMintPosition', [
+      // For the pool creation+initialization
+      pool.token0.address,
+      pool.token1.address,
+      pool.fee,
+      toHex(pool.sqrtRatioX96),
+      // For the position minting
+      position.tickLower,
+      position.tickUpper,
+      toHex(amount0),
+      toHex(amount1),
+      // Slippage shouldn't be an issue as we're creating the pool + mint in a single transaction
+      toHex(0),
+      toHex(0),
+      validateAndParseAddress(recipient),
+      toHex(deadline)
+    ])
+  }
 
   public static createCallParameters(pool: Pool): CallData[] {
     return [this.encodeCreate(pool)]
+  }
+
+  public static createAndMintCallParameters (position: Position, recipient: string, deadline: number): CallData[] {
+    return [
+      PoolInitializer.encodeDeposit(position.pool.token0, position.amount0.quotient),
+      PoolInitializer.encodeDeposit(position.pool.token1, position.amount1.quotient),
+      PoolInitializer.encodeCreateAndMint(position, recipient, deadline)
+    ]
   }
 }
--- packages/sdk/test/poolInitializer.test.ts
+++ packages/sdk/test/poolInitializer.test.ts
@@ -9,6 +9,7 @@ import { encodeSqrtRatioX96 } from '../src/utils/encodeSqrtRatioX96'
 import { maxLiquidityForAmounts } from '../src/utils/maxLiquidityForAmounts'
 import { nearestUsableTick } from '../src/utils/nearestUsableTick'
 import { priceToClosestTick } from '../src/utils/priceTickConversions'
+import { TickMath } from '../src/utils/tickMath'
 
 describe('PoolInitializer', () => {
   // Create two tokens
@@ -85,6 +86,89 @@ describe('PoolInitializer', () => {
         liquidity: liquidity
       }),
       { recipient, slippageTolerance, deadline }
+    )
+  })
+
+  it('create initialize mint', () => {
+    // Init price: 1 token0 = 600 token1
+    const sqrtRatioX96 = encodeSqrtRatioX96(600, 1)
+    
+    // Provide liquidity between 500 and 800
+    const lowerBoundPrice = 500
+    const higherBoundPrice = 800
+    
+    const tickLower = nearestUsableTick(priceToClosestTick(new Price(token0, token1, 1, lowerBoundPrice)), TICK_SPACINGS[fee])
+    const tickUpper = nearestUsableTick(priceToClosestTick(new Price(token0, token1, 1, higherBoundPrice)), TICK_SPACINGS[fee])
+    
+    // Create a virtual pool
+    const pool = new Pool(token0, token1, fee, sqrtRatioX96, 0, TickMath.getTickAtSqrtRatio(sqrtRatioX96))
+    
+    const EXA = JSBI.BigInt(10**18)
+    const amount0 = JSBI.multiply(EXA, JSBI.BigInt(5)) // 5 token0
+    const amount1 = MaxUint256 // compute amount1 needed
+    
+    // Compute the max liquidity given the amounts
+    const liquidity = maxLiquidityForAmounts (
+      pool.sqrtRatioX96, // Pool price
+      encodeSqrtRatioX96(lowerBoundPrice, 1), // lower bound price
+      encodeSqrtRatioX96(higherBoundPrice, 1), // upper bound price
+      amount0, amount1, // amounts of token provided
+      true
+    )
+
+    const position = new Position({
+      pool,
+      tickLower,
+      tickUpper,
+      liquidity
+    })
+
+    // adress that will receive the position NFT
+    const recipient = 'hx0000000000000000000000000000000000000003'
+    const deadline = 123
+    
+    // Initialize the pool + mint position
+    const calldatas = PoolInitializer.createAndMintCallParameters(position, recipient, deadline)
+    
+    expect(calldatas[0]).toStrictEqual({
+      "method": "transfer",
+      "params": {
+          "_data": "0x7b226d6574686f64223a226465706f736974222c22706172616d73223a7b7d7d",
+          "_to": "PoolInitializer",
+          "_value": "0x452d3f9ea92358de"
+      },
+      "to": "cx0000000000000000000000000000000000000001"
+    })
+    
+    expect(calldatas[1]).toStrictEqual({
+      "method": "transfer",
+      "params": {
+          "_data": "0x7b226d6574686f64223a226465706f736974222c22706172616d73223a7b7d7d",
+          "_to": "PoolInitializer",
+          "_value": "0x692a76b89ac3d067cf"
+      },
+      "to": "cx0000000000000000000000000000000000000002"
+    })
+    
+    expect(calldatas[2]).toStrictEqual(
+      {
+        "to": "PoolInitializer",
+        "method": "createAndInitializePoolIfNecessaryAndMintPosition",
+        "params": {
+            "token0": "cx0000000000000000000000000000000000000001",
+            "token1": "cx0000000000000000000000000000000000000002",
+            "fee": "0xbb8",
+            "sqrtPriceX96": "0x187eb1990b697a1c43e9f593ea",
+            "tickLower": "0xf2d0",
+            "tickUpper": "0x10518",
+            "amount0": "0x452d3f9ea92358de",
+            "amount1": "0x692a76b89ac3d067cf",
+            "amount0Min": "0x0",
+            "amount1Min": "0x0",
+            "recipient": "hx0000000000000000000000000000000000000003",
+            "deadline": "0x7b"
+        }
+      }
     )
   })
 })
